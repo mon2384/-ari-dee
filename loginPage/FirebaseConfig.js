2bcead5230d854f26e94128c7ec307629052e981
mon2384 <bar2384@hotmail.com>
2022-04-19 14:52:50 +0700

    add
     fire

---
+++ loginPage/FirebaseConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAy9C07hdwVcteyIHdu-yyls1p3isIcWeY",
+  authDomain: "zcg12-65.firebaseapp.com",
+  databaseURL: "https://zcg12-65-default-rtdb.firebaseio.com",
+  projectId: "zcg12-65",
+  storageBucket: "zcg12-65.appspot.com",
+  messagingSenderId: "989333029174",
+  appId: "1:989333029174:web:e2307d114c22a3291db397",
+  measurementId: "G-L9J5Z896G0"
+};
+
+firebase.initializeApp(firebaseConfig);
--- loginPage/login.js
+++ loginPage/login.js
@@ -1,1 +1,28 @@
-var email = document.getElementById('email').value
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
+
+ var email,pass;
+function set(){
+    email = document.getElementById('email').value;
+    pass = document.getElementById('password').value;
+}
+var lgbt = document.getElementById('lgbtn');
+lgbt.addEventListener('click',loginnowforfree)
+function loginnowforfree(){
+    set()
+    auth.signInWithEmailAndPassword(email, pass)
+    .then((userCredential) =>  {
+        console.log("Successfully Sign in");
+        alert('123')
+
+
+    }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("error " + errorCode + " : " + errorMessage);
+
+    })
+    
+}
