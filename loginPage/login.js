6eb76166236b352ed44c8707c03e28773f25c8bf
mon2384 <bar2384@hotmail.com>
2022-04-19 16:05:30 +0700

    login and reg

--- loginPage/login.js
+++ loginPage/login.js
@@ -1,28 +1,98 @@
-const auth = firebase.auth();
-const db = firebase.firestore();
+const db = firebase.firestore()
+const auth = firebase.auth()
+var user = auth.currentUser
+let loginButton = document.getElementById("lgbtn")
 
 
-
- var email,pass;
-function set(){
-    email = document.getElementById('email').value;
-    pass = document.getElementById('password').value;
+function goreg(){
+    window.location.href = '../Register.html'
 }
-var lgbt = document.getElementById('lgbtn');
-lgbt.addEventListener('click',loginnowforfree)
-function loginnowforfree(){
-    set()
-    auth.signInWithEmailAndPassword(email, pass)
-    .then((userCredential) =>  {
-        console.log("Successfully Sign in");
-        alert('123')
 
+/*function REG() {
+    var email = document.getElementById("emailField").value; /// ตัวแปร pass จะเก็บข้อมูล String จากกล่องข้อความ ที่มี ID emailField
+    var pass = document.getElementById("passField").value; /// ตัวแปร pass จะเก็บข้อมูล String จากกล่องข้อความ ที่มี ID passField
+  
+    auth.createUserWithEmailAndPassword(email, pass)
+    .then((userCredential) => { 
 
-    }).catch((error) => {
+      user = userCredential.user;
+
+      console.log("Successfully Registered with (" + email + ")" );
+      
+
+      db.collection("EATRAIDEE").doc(email).collection("UserInfo").doc("userLogin").set({
+        Email : email,
+        UID : user.uid,
+        Password : pass
+      
+      });
+  
+      })
+      .catch((error) => { ///ส่วนนี้ ใช้แสดง Error ขึ้นบน console.log
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log("error " + errorCode + " : " + errorMessage);
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `WTF WITH EMAIL`,
+        })
+      });
+  }
+  */
+  function Loginnow() {
 
+    var email = document.getElementById("emailField").value;
+    var pass = document.getElementById("passField").value;
+  
+    auth.signInWithEmailAndPassword(email, pass)
+      .then((userCredential) => {
+        user = userCredential.user;
+  
+        console.log("Successfully Sign in");
+        Swal.fire({
+            icon: 'success',
+            title: `Welcome ${email}`,
+            text: `Havefun`,
+
+        })
+        
+        openrl()
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("error " + errorCode + " : " + errorMessage);
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `Don't Have This User In Database OR Password is invalid`,})
+      });
+  
+  }
+  
+  function Logout() {
+  
+    auth.signOut() //<<< นี่คือคำสั่ง Logout
+    .then(() => {
+        var email = document.getElementById("emailField").value;
+      console.log("Successfully Sign out");
+
+      Swal.fire({
+        icon: 'success',
+        title: 'lol...',
+        text: `GOODBYE ${email}`,})
+        
+  
     })
-    
-}
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("error " + errorCode + " : " + errorMessage);
+    });
+  }
+
+
+  function openrl(){
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  }
