79ff7bb1e52277f97fe057ba2ed2769ac2eb5ae4
khunponKU <khunpon.s@ku.th>
2022-04-22 10:08:26 +0700

    create Menu page

---
+++ Menupage/firebaseconfigMenu.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgKqMLNsVigHq2_7ihDd28sRGZestbtVU",
+    authDomain: "test-gin-ari-dee.firebaseapp.com",
+    databaseURL: "https://test-gin-ari-dee-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "test-gin-ari-dee",
+    storageBucket: "test-gin-ari-dee.appspot.com",
+    messagingSenderId: "593733968194",
+    appId: "1:593733968194:web:b554dd68b17648f4f6a8db",
+    measurementId: "G-T6T2ZT5B3S"
+  };
---
+++ Menupage/scripMenu.js
@@ -0,0 +1,7 @@
+const db = firebase.firestore()
+document.getElementById('username').innerText = "Username : " + localStorage.getItem('USERNAME')
+
+// function img
+function myFunction(imgs) {
+    
+  }
